34b1b71856b41a179b399b2a72f6fd4ab321354f
choiseonkang <com1368@naver.com>
2023-09-22 00:32:15 +0900

    feat : 기본 세팅

---
+++ src/main/kotlin/com/example/practicekotlin/PracticeKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.practicekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<PracticeKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/practicekotlin/PracticeKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.practicekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
