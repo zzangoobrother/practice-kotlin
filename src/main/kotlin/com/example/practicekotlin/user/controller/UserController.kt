0cd752fef07ffd48eefe803fafa7f204ea8ddc1e
choiseonkang <com1368@naver.com>
2023-10-18 23:40:33 +0900

    feat : log 테스트

--- src/main/kotlin/com/example/practicekotlin/user/controller/UserController.kt
+++ src/main/kotlin/com/example/practicekotlin/user/controller/UserController.kt
@@ -4,6 +4,8 @@ import com.example.practicekotlin.user.dto.LoginRequest
 import com.example.practicekotlin.user.dto.SignupRequest
 import com.example.practicekotlin.user.entity.UserEntity
 import com.example.practicekotlin.user.service.UserService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,12 +20,20 @@ class UserController(
     private val userService: UserService,
 ) {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     fun signup(
         @Valid @RequestBody
         request: SignupRequest,
     ): Long {
+        log.trace("signup trace")
+        log.debug("signup debug")
+        log.info("signup info")
+        log.warn("signup warn")
+        log.error("signup error")
+
         return userService.signup(request)
     }
 
